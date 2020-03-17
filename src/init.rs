bc99034974278fd8455b4bf87b472589d8a08f9f
Lu Xinyan <iamalexlu@gmail.com>
2020-03-17 23:48:16 +0800

    implement macro of print! and prinln!

--- src/init.rs
+++ src/init.rs
@@ -1,12 +1,17 @@
 global_asm!(include_str!("boot/entry64.asm"));
 
-use crate::sbi;
-
 #[no_mangle]
 extern "C" fn rust_main() -> ! {
-    sbi::console_putchar(b'O' as usize);
-    sbi::console_putchar(b'K' as usize);
-    sbi::console_putchar(b'\n' as usize);
+    extern "C" {
+        fn _start();
+        fn bootstacktop();
+    }
+
+    println!("_start vaddr = 0x{:x}", _start as usize);
+    println!("bootstacktop vaddr = 0x{:x}", bootstacktop as usize);
+    println!("hello world!");
+
+    panic!("you want to do nothing!");
 
     loop {}
 }
